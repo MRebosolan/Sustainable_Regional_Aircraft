399da0d78bdd8c0f6b21e0e19b12e12917641ea3
mrebosolan <superreba17@gmail.com>
2020-05-25 16:18:54 +0200

    more tail comments ffs

--- Tail_weight_estimation.py
+++ Tail_weight_estimation.py
@@ -4,7 +4,8 @@ from math import radians, cos
 def hor_tail_weight(Kh, Sh, VD, half_chord_sweep_hor):
     #Already verified via hand calculation
     #Kh=1 for fixed incidence stabilizers, 1.1 for variable incidence stabilizers
-    #VD= dive speed in KEAS
+    #VD= dive speed in KEAS (Knots Equivalent Air Speed)
+    #----------> USE IMPERIAL!!!!!!!! <-------------
 
     hor_tail_weight= Kh * Sh * ((3.81*(Sh**0.2 * VD)) / (1000 * (cos(radians(half_chord_sweep_hor)))**0.5)-0.287)
     return hor_tail_weight
