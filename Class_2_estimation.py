341f5a14938b456701406d278942450adb94faaf
Jornvanbeek <jornvanbeek143@gmail.com>
2020-05-25 14:07:53 +0200

    class 2 itemize all parameters

--- Class_2_estimation.py
+++ Class_2_estimation.py
@@ -1,1 +1,38 @@
-#do you see this jari?
+OEW = 
+
+structural = 
+wing_group
+tail_group
+body_group
+landing_group
+control_group
+nacelle_group
+
+propulsion_group =
+engine_installation
+air_induction
+exhaust
+power_controls
+fuel_system
+thrust_reverser
+
+equipment=
+apu
+instrumentation
+hydraulics
+fuel_system_2
+air_conditioning
+furnishing
+
+furnishing =
+flight_deck
+cabin_accomodations
+emergency_equipment
+
+operational_items =
+crew
+supplies
+water
+safety_equipment
+trapped_fuel
+cargo_equipment
