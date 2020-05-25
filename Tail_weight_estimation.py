516b898bcf61090a4743e35edab92f587750c38a
mrebosolan <superreba17@gmail.com>
2020-05-25 15:25:59 +0200

    tail estimation completed

--- Tail_weight_estimation.py
+++ Tail_weight_estimation.py
@@ -9,10 +9,13 @@ def hor_tail_weight(Kh, Sh, VD, half_chord_sweep_hor):
     hor_tail_weight= Kh * Sh * ((3.81*(Sh**0.2 * VD)) / (1000 * (cos(radians(half_chord_sweep_hor)))**0.5)-0.287)
     return hor_tail_weight
 
-def hor_tail_weight(Kh, Sh, VD, half_chord_sweep_hor):
-    #Already verified via hand calculation
-    #Kh=1 for fixed incidence stabilizers, 1.1 for variable incidence stabilizers
-    #VD= dive speed in KEAS
+def Kv_fin_mounted(Sh, zh, Sv, bv):
+    Kv = (1 + 0.15*((Sh*zh) / (Sv*bv)))
+    return Kv
 
-    hor_tail_weight= Kh * Sh * ((3.81*(Sh**0.2 * VD)) / (1000 * (cos(radians(half_chord_sweep_hor)))**0.5)-0.287)
-    return hor_tail_weight
+def vert_tail_weight(Kv, Sv, VD, half_chord_sweep_vert):
+    #Already verified via hand calculation
+    #Kv=1 for fuselage mounted hor tails, use Kv_fin_mounted for fin mounted hor tails
+    vert_tail_weight = Kv * Sv * ((3.81 * (Sh ** 0.2 * VD)) / (1000 * (cos(radians(half_chord_sweep_vert))) ** 0.5) - 0.287)
+    return vert_tail_weight
+
