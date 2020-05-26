1c2bd1067ec1008ee76006fc051a63ff7fad4867
mrebosolan <superreba17@gmail.com>
2020-05-26 17:00:59 +0200

    import statements

--- Class_2_estimation.py
+++ Class_2_estimation.py
@@ -1,4 +1,28 @@
-OEW = 
+import APU_weight_estimation as apu
+import Cargo_handling_weight_estimation as cargo
+import electrical_system_weight_estimation as electrical
+import Engine_weight_estimation as engine
+import Envelope as envelope
+import flight_controls_weight_estimation as flightcontrols
+import fuel_system_weight_estimation as fuelsystem
+import furnishing_weight_estimation as furnishing
+import fuselage_weight_estimation as fuselage
+import instrumentation_weight_estimation as instrumentation
+import LG_weight_estimation as LG
+import nacelle_weight_estimation as nacelles
+import Oxygen_system_weight_estimation as oxygen
+import paint_weight_estimation as paint
+import power_controls_weight_estimation as powercontrols
+import Pressurization_system_weight_estimation as pressurization
+import Tail_weight_estimation as tail
+import Wing_weight_estimation as wing
+
+
+
+
+
+
+OEW =
 
 structural = 
 wing_group
