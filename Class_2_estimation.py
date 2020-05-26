06ce631019f8be2e0f821f2553ae92d315b004ee
Jornvanbeek <jornvanbeek143@gmail.com>
2020-05-26 16:59:49 +0200

    moved a lot of shit, wrote down equation for total weight

--- Class_2_estimation.py
+++ Class_2_estimation.py
@@ -1,49 +1,74 @@
-OEW = 
-
-structural = 
-wing_group
-tail_group
-body_group
-landing_group
-nacelle_group
-
- #still missing
-
-propulsion_group =
-engine_installation #matteo
-
-fuel_system 
-power_controls 
-
-equipment=
-
-flight_control
-hydraulics #see appendix A
-electrical system
-instrumentation #jorn
-airconditioning_pressurization
-oxygen_system
-APU
-furnishing
-baggage_cargo
-operational_items
-ballast
-paint
-
-misc
 
 
-# furnishing =
-# flight_deck
-# cabin_accomodations
-# emergency_equipment
 
 
-# operational_items =
-# crew
-# supplies
-# water
-# safety_equipment
-# trapped_fuel
-# cargo_equipment
+
+
+
+
+
+
+
+
+
+W_struct = wing + empennage + fuselage + nacelles + landing_gear
+
+W_powerplant = engines + fuel_system + power_controls + 
+
+W_equipment = APU + cargo_equipment + furnishing + instrumentation + oxygen_system + paint + pressurization + flight_control
+
+
+
+
+
+
+
+# OEW = 
+
+# structural = 
+# wing_group
+# tail_group
+# body_group
+# landing_group
+# nacelle_group
+
+#  #still missing
+
+# propulsion_group =
+# engine_installation #matteo
+
+# fuel_system 
+# power_controls 
+
+# equipment=
+
+# flight_control
+# hydraulics #see appendix A
+# electrical system
+# instrumentation #done
+# airconditioning_pressurization# done
+# oxygen_system #done
+# APU # matteo
+# furnishing #jorn
+# baggage_cargo
+# operational_items #included in furnishing
+# ballast #tbd
+# paint
+
+# misc
+
+
+# # furnishing =
+# # flight_deck
+# # cabin_accomodations
+# # emergency_equipment
+
+
+# # operational_items =
+# # crew
+# # supplies
+# # water
+# # safety_equipment
+# # trapped_fuel
+# # cargo_equipment
 
