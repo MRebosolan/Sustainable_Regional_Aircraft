d6a98ddd34259c3523a418ad00bb311afcd34b55
mrebosolan <superreba17@gmail.com>
2020-05-26 15:23:21 +0200

    engine weight

--- Engine_weight_estimation.py
+++ Engine_weight_estimation.py
@@ -1,1 +1,16 @@
-#To do, somehow find regression for figure 6.1 in roskam weight estimation
+import numpy as np
+from math import log
+
+#Regression on data obtained from http://www.jet-engine.net/civtfspec.html
+
+
+thrust = np.array([6500, 7580, 6442, 7500, 7000, 8650, 13790, 14500, 18000])
+dry_weights= np.array([1364, 1586, 1586, 1311, 1385, 1625, 2350, 2470, 3800])
+
+A, B = np.polyfit(np.log(thrust), dry_weights,1)
+
+
+def engine_weight(dry_thrust_SL, num_engines):
+    engine_weight= A * log(dry_thrust_SL) + B
+    return engine_weight * num_engines
+
