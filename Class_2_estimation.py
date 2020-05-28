085cd0d46805918b1a8856283853b2fe7c2a84e2
mrebosolan <superreba17@gmail.com>
2020-05-28 14:28:30 +0200

    pippo

--- Class_2_estimation.py
+++ Class_2_estimation.py
@@ -192,7 +192,8 @@ wng = [{'data': 'Wing group', 'SRA': W_wing, 'F28':7330},
        {'data': 'Fuselage', 'SRA': W_fuselage, 'F28':7043},
        {'data': 'Nacelle', 'SRA': W_nacelles, 'F28':834},
        {'data': 'Landing gear', 'SRA': W_landing_gear, 'F28':2759},
-       {'data': 'Total structural', 'SRA': W_struct, 'F28':19598}]
+       {'data': 'Total structural', 'SRA': W_struct, 'F28':19598},
+       {'data': 'Total fixed equipment', 'SRA': W_equipment, 'F28':9395}]
 df = df.append(wng, ignore_index = True, sort = False)
 
 
