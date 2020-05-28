5ba68f4abb74b7129588b6c0cfbae1c465333f5a
Jornvanbeek <jornvanbeek143@gmail.com>
2020-05-28 10:37:11 +0200

    fixing fuselage weight

--- fuselage_weight_estimation.py
+++ fuselage_weight_estimation.py
@@ -2,13 +2,13 @@
 from math import *
 
 
-def dynamic_pressure(rho, V):
-    q = 0.5*rho* V*V
-    q_psf = q * 0.020885
-    return q, q_psf
+# def dynamic_pressure(rho, V):
+#     q = 0.5*rho* V*V
+#     q_psf = q * 0.020885
+#     return q, q_psf
 
-def W_fuselage_gd (rho, V_dive, MTOW, lf, hf): #V_dive in m/s
-    q_d = dynamic_pressure(rho, V_dive)[1]
+def W_fuselage_gd (rho_zero, V_dive, MTOW, lf, hf): #V_dive in m/s
+    q_d = V_dive*V_dive*rho_zero / 2
     return 10.43 * ((q_d/100)**0.283) * ((MTOW/1000)**0.95)*((lf/hf)**0.71) # q is design dive dynamic pressure, MTOW in lbs, lf and hf in feet
 
 def W_fuselage_torenbeek(V_d, lh, wf, hf, S_fgs): #v_dive in keas, lh = distance from wing root quarter chord to horizontal tail root quarter chord
