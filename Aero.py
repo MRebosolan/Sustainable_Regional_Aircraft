280f0fc84785ade0a456a5e86e70771983e435fb
georgetzanetos <tzortzistzanetos@gmail.com>
2020-06-02 10:06:25 +0200

    Create Aero.py

---
+++ Aero.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+inputs
+    CLmax
+    Altitude
+    Equivalent Airspeed
+    Wing Loading
+    Class I Weight Estimation Outputs
+
+outputs 
+    Wing Area
+    Wing Sweep
+    Empennage Area 
+    Dihedral Angle
+    
+Description 
+    This script calculates the main aerodynamic values that determine the wing and tail configuration of the aircraft.
+
+"""
