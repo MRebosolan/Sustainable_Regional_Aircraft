9ce43274721233ebddcedc5cca1edf3675fe08c6
Montse Barbera <montsebarbera@MacBook-Pro-de-Montse.local>
2020-06-02 10:26:31 +0200

    wingbox design parameters

--- Wingbox_design.py
+++ Wingbox_design.py
@@ -1,3 +1,13 @@
 import input
 
-winglaoding = input.wingloading
+winglaoding = input.wingloading #import wingloading
+
+b = input.b #import wingspan
+S = input.S #import wing area
+AR = input.AR #aspect ratio
+taper = input.taper #input taper
+t_r = input.t_r # input t_r
+SMC = b/AR #standar mean chord
+
+Load_distribution_lift = SMC*winglaoding
+load_distribution_wingweight =
