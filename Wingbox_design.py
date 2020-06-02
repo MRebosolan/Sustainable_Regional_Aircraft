5eb989ef40a1d0c9062a0be9514a0c0cef8997d3
mrebosolan <superreba17@gmail.com>
2020-06-02 11:51:23 +0200

    elliptical lift d + reaction forces

--- Wingbox_design.py
+++ Wingbox_design.py
@@ -1,4 +1,5 @@
 import input
+from math import *
 
 wingloading = input.wingloading #import wingloading
 
@@ -9,11 +10,27 @@ taper = input.taper #input taper
 t_r = input.t_r # input t_r
 SMC = b/AR #standar mean chord
 widthf = input.widthf
+wing_length = 0.5*(b-widthf)
 
 lift_d = SMC*wingloading #not yet adapted to triangle
 w_wingd = 1000 # wing weight distribution
 
-#section 1-2 wintip to engine
+def elliptical_lift_d (x, a=10.0135, b=40957.2):
+    #obtained by fitting elliptical distribution to given wing loading, a=half wing span - half fuselage width
+    loading_at_x = sqrt((1-(x**2/a**2))*b**2)
+    return loading_at_x
+
+
+x_lift = 4.2499 #application point of lift force
+
+
+def wing_root_reaction_forces (L_wing, x_lift, W_wing, x_weight, W_engine, x_engine):
+    R_y = W_wing + W_engine - L_wing  #upwards positive
+    M = x_lift*L_wing - x_weight*W_wing - x_engine*W_engine #counterclockwise positive
+    return (R_y, M)
+
+
+#section 1-2 wingtip to engine
 
 
 
