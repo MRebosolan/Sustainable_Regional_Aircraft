c613ba6588a2c9a1a5b8fd60caece6a0ba4d712d
Montse Barbera <montsebarbera@MacBook-Pro-de-Montse.local>
2020-06-02 14:34:06 +0200

    wingbox

--- Wingbox_design.py
+++ Wingbox_design.py
@@ -19,14 +19,17 @@ w_wingd = 1000 # wing weight distribution
 w_engine = 10 # engine weight
 x_engine = 3 # engine distance from tip
 
+
+
+
 x_loc = []
 y_shear = []
 y_moment = []
 
 for i in range(1,100):
-    x = b*(i/100)
+    x = wing_length*(i/100)
     if x<x_engine:
-        Shear_Force = lift_d*x**2/b - w_wingd*x
+        Shear_Force = lift_d*x**2/(2*wing_length) - w_wingd*x
         Moment = lift_d*x**3/(3*b) - w_wingd*x**2/2
     else:
         Shear_Force = lift_d*x**2/b - w_wingd*x - w_engine
