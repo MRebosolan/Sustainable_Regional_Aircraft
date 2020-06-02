b5f3f937619b53c284e1f46de4c94f282a80f960
martijnvroom <martijnv17@gmail.com>
2020-06-02 10:23:05 +0200

    -

--- Aero.py
+++ Aero.py
@@ -1,4 +1,5 @@
 import numpy as np
+import input as inp
 import matplotlib.pyplot as plt
 
 """
@@ -19,3 +20,18 @@ Description
     This script calculates the main aerodynamic values that determine the wing and tail configuration of the aircraft.
 
 """
+
+M_cruise = 0.6
+S = inp.S
+AR = inp.AR
+
+if M_cruise >= 0.7:
+    sweep_c4 = np.acos(0.935/(0.03 + M_cruise))
+else
+    sweep_c4 = np.acos(1)
+
+taper = 0.2 * (2 - sweep_c4)
+
+b = np.sqrt(S*AR)
+c_root = 2*S / ((1 + taper)*b)
+c_tip = taper * c_root
