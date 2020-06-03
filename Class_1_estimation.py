971b3a1898daef477ef5d4235e49c525ee830a06
AQUA7XICE <61149150+AQUA7XICE@users.noreply.github.com>
2020-06-03 12:31:52 +0200

    Revert "gg"
    This reverts commit 9c6f4751871ac4b39a89f8fe242e637f31ec82bd.

--- Class_1_estimation.py
+++ Class_1_estimation.py
@@ -94,7 +94,7 @@ from hydrogen_tank_sizing import tank_sizing
 
 
 def CLASS1WEIGHTHYBRID(H_to_ker_ratio = input.H_to_ker_ratio,OEWINPUT = 1):
-    W_hydrosys=H_to_ker_ratio*2000
+    W_hydrosys=H_to_ker_ratio*3000
     e=2.71828182846
     n_pax= input.Npax
     W_pax= input.W_pax
@@ -229,69 +229,70 @@ for i in range(0,101):
     xlist.append(i)
 
 
-plt.subplot(3,2,1)
+plt.subplot(3,3,1)
 plt.plot(xlist,mtowlist,label='MTOW')
 plt.plot(xlist,oewlist,label='OEW')
 #plt.plot([0,100],[28992,28992],label='MTOW kerosene reserve frac')
 #plt.plot([0,100],[18273,18273],label='OEW kerosene reserve frac')
+
 plt.ylabel('WEIGHT [kg]')
-plt.xlabel('Hydrogen mass ratio as %')
+plt.xlabel('%MASS OF HYDROGEN IN MIXTURE')
 plt.legend()
 
-plt.subplot(3,2,2)
+plt.subplot(3,3,2)
 plt.plot(xlist,kerosenelist,label='kerosene mass')
 plt.plot(xlist,hydrogenlist,label='hydrogen mass')
 plt.plot(xlist,tfuellist,label='total fuel mass')
 #plt.plot([0,100],[660,660],label='kerosene in kerosene reserve frac')
 #plt.plot([0,100],[2084,2084],label='hydrogen in kerosene reserve frac')
 #plt.plot([0,100],[2744,2744],label='total fuel in kerosene reserve frac')
-plt.ylabel('WEIGHT [kg]')
-plt.xlabel('Hydrogen mass ratio as %')
-plt.legend()
 
-plt.subplot(3,2,3)
+plt.ylabel('WEIGHT [kg]')
+plt.xlabel('%MASS OF HYDROGEN IN MIXTURE')
+plt.legend()
+plt.subplot(3,3,3)
 plt.plot(xlist,hvollist,label='Hydrogen volume')
 plt.ylabel('Volume [CUBIC METERS]')
-plt.xlabel('Hydrogen mass ratio as %')
+plt.xlabel('%MASS OF HYDROGEN IN MIXTURE')
 plt.legend()
 
-#plt.subplot(3,3,4)
-#plt.plot(xlist,tdiameterlist,label='Tank diameter')
-#plt.ylabel('Meters')
-#plt.xlabel('%MASS OF HYDROGEN IN MIXTURE')
-#plt.legend()
-#
-#plt.subplot(3,3,5)
-#plt.plot(xlist,tmasslist,label='Tank mass')
-#plt.ylabel('kgs')
-#plt.xlabel('%MASS OF HYDROGEN IN MIXTURE')
-#plt.legend()
-#
-#plt.subplot(3,3,6)
-#plt.plot(xlist,energylist,label='total E carried')
-#plt.plot(xlist, [i*122.8 for i in hydrogenlist],label='E hydrogen')
-#plt.plot(xlist, [i*42.8 for i in kerosenelist],label='E kerosene')
-#plt.ylabel('MJ')
-#plt.xlabel('%MASS OF HYDROGEN IN MIXTURE')
-#plt.legend()
+plt.subplot(3,3,4)
+plt.plot(xlist,tdiameterlist,label='Tank diameter')
+plt.ylabel('Meters')
+plt.xlabel('%MASS OF HYDROGEN IN MIXTURE')
+plt.legend()
 
-plt.subplot(3,2,4)
+plt.subplot(3,3,5)
+plt.plot(xlist,tmasslist,label='Tank mass')
+plt.ylabel('kgs')
+plt.xlabel('%MASS OF HYDROGEN IN MIXTURE')
+plt.legend()
+
+plt.subplot(3,3,6)
+plt.plot(xlist,energylist,label='total E carried')
+plt.plot(xlist, [i*122.8 for i in hydrogenlist],label='E hydrogen')
+plt.plot(xlist, [i*42.8 for i in kerosenelist],label='E kerosene')
+plt.ylabel('MJ')
+plt.xlabel('%MASS OF HYDROGEN IN MIXTURE')
+plt.legend()
+
+plt.subplot(3,3,7)
 plt.plot(xlist,[i*input.hydrogen_cost for i in hydrogenlist],label='Hydrogen cost')
 plt.plot(xlist,[i*0.6/0.81 for i in kerosenelist],label='kerosene cost')
 plt.plot(xlist,[sum(x) for x in zip([i*input.hydrogen_cost for i in hydrogenlist], [i*0.6/0.81 for i in kerosenelist])],label='total cost')
 plt.legend()
 
 plt.ylabel('US DOLLARS')
-plt.xlabel('Hydrogen mass ratio as %')
+plt.xlabel('%MASS OF HYDROGEN IN MIXTURE')
 
-plt.subplot(3,2,5)
+plt.subplot(3,3,8)
 plt.plot(xlist,emissionslist,label='emissions')
-plt.xlabel('Hydrogen mass ratio as %')
+plt.xlabel('%MASS OF HYDROGEN IN MIXTURE')
 plt.legend()
 
-plt.subplot(3,2,6)
+plt.subplot(3,3,9)
 plt.plot(xlist,emissionsratiolist,label='CF RATIO wrt CRJ700')
-plt.xlabel('Hydrogen mass ratio as %')
+plt.xlabel('%MASS OF HYDROGEN IN MIXTURE')
 plt.legend()
 plt.show()
 
