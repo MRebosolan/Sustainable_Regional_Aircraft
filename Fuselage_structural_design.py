a0ded469ed49dad38b6bf51d99f77ef2c5bd7ce5
mrebosolan <superreba17@gmail.com>
2020-06-04 10:04:21 +0200

    fuselage stresses

--- Fuselage_structural_design.py
+++ Fuselage_structural_design.py
@@ -1,11 +1,26 @@
 import input
-
+import numpy as np
 
 P_c = input.P_c
-d = input.widthf
+r = input.widthf/2
 
-def pressure_vessel_stresses(P=P_c, d=d, t):
-    hoop_stress = (P*d)/t
+t_options = np.linspace(0, 0.30, 1000)[1:]
+
+
+def pressure_vessel_stresses(t, P=P_c, r=r):
+    hoop_stress = (P*r)/t
     longitudinal_stress = 0.5 * hoop_stress
     return hoop_stress, longitudinal_stress
+
+hoop_stresses=[]
+longitudinal_stresses=[]
+
+for t in t_options:
+    hoop, long = pressure_vessel_stresses(t)
+    hoop_stresses.append(hoop)
+    longitudinal_stresses.append(long)
+
+
+
+
 
