419c2ba423c266838d24df234cc72f5105aff6b7
RickvanRooijen <61013690+RickvanRooijen@users.noreply.github.com>
2020-06-04 10:31:28 +0200

    cg excursion

---
+++ CG_excursiong_wing_shift.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 09:33:37 2020
+
+@author: Gebruiker
+"""
+
+# loading diagram generator
+
+import matplotlib.pyplot as plt
+import numpy as np
+import Class_2_estimation as cl2
+import input
+from loading_diagram_generator import wing_cg, cg_OEW_wrt_lemac, loadingcg, passenger_loading
+
+#Raw inputs
+MTOW = cl2.MTOM                 #kg
+OEW = cl2.OEM                   #kg
+MZF = cl2.M_zfw_kg              #kg
+Npax = input.Npax               #Number of passengers [-]
+w_person = input.W_pax          #Weight of each passenger + luggage [kg]
+l_f = input.lf                  #Fuselage length [m]
+cargo = input.W_cargo           #Total cargo weight [kg]
+fuel_weight = cl2.M_fuel_kg     #Total fuel weight taken onboard, including reserves [kg]
+MAC = input.MAC                 #Length of MAC [m]
+y_MAC = input.y_MAC             #spanwise location of mean aerodynamic chord, measured from the centerline of the aircraft[m]
+x_lemac_Cr = input.x_lemac_rootchord #x location of leading edge mac measured from root chord [m]
+seat_start = input.x_first_pax  #x-location measured from the nose where first passenger row is located
+pitch = input.seat_pitch        #seat pitch [inch]
+rows = input.n_rows             #number of passegner rows [-]
+lh = input.lh                   #distance between wing and horizontal tail aerodynamic centers
+lv = input.lv                   #distance between wing and vertical tail aerodynamic centers
+x_apu = input.x_apu             #cg location of the apu measured from the nose of the aircraft [m]
+
+Cr = input.Cr                   #wing root chord length [m]
+Ct = input.Ct                   #wing tip chord length [m]
+b = cl2.b                       #wing span [m]
+sweep = input.LE_sweep          #Leading edge wing sweep (If we use forward sweep, please let Rick know)
+x_cargo_fwd = input.x_cg_fwd_cargo #front cargo cg measured from nose [m]
+x_cargo_aft = input.x_cg_aft_cargo #aft cargo cg measured from nose [m]
+pax_abreast = input.pax_abreast
+
+x_start_Cr = np.arange(0.2 * l_f, 0.8 * l_f, 50)   #x-location measured from the nose where root chord starts
+x_lemac = [i + x_lemac_Cr for i in x_start_Cr]
+x_engine = x_lemac       #Assume engine cg is at lemac
+x_nacelle = x_engine     #assume nacelle cg is at engine cg
+x_ac = [i + 0.25 * MAC for i in x_lemac]            #assume ac at quarter chord point
+#Small calculations with raw inputs
+pax_cabin = Npax * w_person
+fwd_cargo_max = cargo * input.cargo_fwd_fraction
+aft_cargo_max = cargo * input.cargo_aft_fraction
+
+seatloc = []
+rows = Npax/pax_abreast
+for j in range(int(rows)):
+    row = seat_start + j * pitch * 0.0254  # convert to meters
+    seatloc.append(row)
+
+
+
+
+#Calculate x_cg & OEW
+w_engine = cl2.df['SRA']['Engines']  # kg
+w_nacelle = cl2.df['SRA']['Nacelle']  # kg  
+w_empennage = cl2.df['SRA']['Empennage']    #kg
+w_wing = cl2.df['SRA']['Wing group'] #kg 
+w_apu = cl2.df['SRA']['APU']    #kg
+w_tank = 500
+x_tank = 20
+print("change w_tank and x_tank to variables used in other files once decided on a fuel tank configuration")
+x_fuel = x_tank                 #fuel cg measured from nose, assumed same as tank cg as most likely the tank will be symmetrical
+w_lg_main = cl2.df['SRA']['Main LG']    #kg
+w_lg_front = cl2.df['SRA']['Nose LG']    #kg
+
+
+x_empennage = [i + (lh + lv) / 2 for i in x_ac] #Assume cg of empennage is in the middle of the aerodynamic center of horizontal and vertical tail, measured from the nose
+x_lg_front = 3     #cg location of front landing gear [m], measured from the nose, assumed to be 3 m (used for calculating cg at oew, not to be changed per se)
+x_lg_main = [i + 2 * Cr / 3 for i in x_start_Cr]     #cg location of main landing gear [m], assumed 2/3 root chord length further than start of root chord (used for calculating cg at oew, not to be changed per se)
+print("In calculation of cg @ OEW, take into account the exact tank placement and cg location once agreed on a specific configuration")
+
+
+def cg_excursion_wing_shift():
+    cg_fwd_excursion_lst = []
+    cg_aft_excursion_lst = []
+    for i in range(len(x_start_Cr)):
+        x_cg_wing_nose = wing_cg(sweep, b, Cr, Ct, MAC, x_lemac_Cr, x_lemac[i])[0]
+        cg_oew_nose = cg_OEW_wrt_lemac(x_engine[i], w_engine, x_nacelle[i], w_nacelle, x_empennage[i], w_empennage, x_apu, w_apu, x_tank, w_tank, x_cg_wing_nose, w_wing, x_lg_front, w_lg_front, x_lg_main, w_lg_main, OEW, x_lemac[i], MAC)[1]
+        onlyfwdcargo = loadingcg(OEW, cg_oew_nose, fwd_cargo_max, x_cargo_fwd)
+        onlyaftcargo = loadingcg(OEW, cg_oew_nose, aft_cargo_max, x_cargo_aft)
+        bothcargo = loadingcg(onlyfwdcargo[1], onlyfwdcargo[0], aft_cargo_max, x_cargo_aft)
+        window = passenger_loading(bothcargo[1], bothcargo[0], multiplication=2)
+        window_back = passenger_loading(bothcargo[1], bothcargo[0], multiplication=2, seatloc=seatloc[::-1])
+        middle = passenger_loading(window[1][-1], window[0][-1], multiplication=2)
+        middle_back = passenger_loading(window[1][-1], window[0][-1], multiplication=2, seatloc=seatloc[::-1])
+        aisle = passenger_loading(middle[1][-1], middle[0][-1])
+        aisle_back = passenger_loading(middle[1][-1], middle[0][-1], seatloc=seatloc[::-1])
+        fully_loaded = loadingcg(aisle[1][-1], aisle[0][1], fuel_weight, x_fuel)
+        cg_excursion = np.array([[onlyfwdcargo[0]], [onlyaftcargo[0]], [bothcargo[0]], [window[0]], window_back[0], 
+                             middle[0], middle_back[0], aisle[0], aisle_back[0], fully_loaded[0]]) 
+        cgmin_lst = []
+        cgmax_lst = []
+        for i in range(len(cg_excursion)):
+            cgmin = np.min(cg_excursion[i])
+            cgmax = np.max(cg_excursion[i])
+            cgmin_lst.append(cgmin)
+            cgmax_lst.append(cgmax)
+            
+        
+        cg_fwd = (np.min(cgmin_lst) - x_lemac) / MAC * 0.98      #subtract 2% margin, assuming most forward cg is after lemac
+        cg_aft = (np.max(cgmax_lst) - x_lemac) / MAC * 1.02      #add 2% margin
+        cg_fwd_excursion_lst.append(cg_fwd)
+        cg_aft_excursion_lst.append(cg_aft)
+    plt.plot(cg_fwd_excursion_lst, x_lemac / l_f, cg_aft_excursion_lst, x_lemac / l_f)
+    plt.show()
+    
+    return 
+cg_excursion_wing_shift()
+
+
+
+
+
--- loading_diagram_generator.py
+++ loading_diagram_generator.py
@@ -64,8 +64,7 @@ x_fuel = x_tank                 #fuel cg measured from nose, assumed same as tan
 w_lg_main = cl2.df['SRA']['Main LG']    #kg
 w_lg_front = cl2.df['SRA']['Nose LG']    #kg
 
-x_engine = 15       #x_location of c.g. of engines measured from the nose [m]
-x_nacelle = 15      #x_location of c.g. of engine nacelles measured from the nose [m]
+
 x_empennage = x_ac + (lh + lv) / 2 #Assume cg of empennage is in the middle of the aerodynamic center of horizontal and vertical tail, measured from the nose
 x_lg_front = 3     #cg location of front landing gear [m], measured from the nose, assumed to be 3 m (used for calculating cg at oew, not to be changed per se)
 x_lg_main = x_start_Cr + 2 * Cr / 3      #cg location of main landing gear [m], assumed 2/3 root chord length further than start of root chord (used for calculating cg at oew, not to be changed per se)
@@ -135,8 +134,8 @@ def passenger_loading(current_weight, current_cg, multiplication=1, seatloc=seat
     return cglist, weight_list
 
 
-# plot potato diagram
 def loading():
+
     plt.close()
     plt.figure()
     onlyfwdcargo = loadingcg(OEW, cg_oew_nose, fwd_cargo_max, x_cargo_fwd)
@@ -173,174 +172,24 @@ def loading():
     plt.ylabel('mass [kg]')
     plt.xlabel('xcg [% of MAC]')
     plt.show()
-    
     cg_excursion = np.array([[onlyfwdcargo[0]], [onlyaftcargo[0]], [bothcargo[0]], [window[0]], window_back[0], 
-                             middle[0], middle_back[0], aisle[0], aisle_back[0], fully_loaded[0]]) - x_lemac / MAC
-    cg_fwd = np.min(cg_excursion) * 0.98        #subtract 2% margin
-    cg_aft = np.max(cg_excursion) * 1.02        #add 2% margin
-    print(cg_fwd,cg_aft)
-    #extreme_cg = maccie(onlyfwdcargo[0], x_lemac, MAC), maccie(onlyaftcargo[0], x_lemac, MAC)
-    return fully_loaded[0], extreme_cg
+                             middle[0], middle_back[0], aisle[0], aisle_back[0], fully_loaded[0]]) 
+    cgmin_lst = []
+    cgmax_lst = []
+    for i in range(len(cg_excursion)):
+        cgmin = np.min(cg_excursion[i])
+        cgmax = np.max(cg_excursion[i])
+        cgmin_lst.append(cgmin)
+        cgmax_lst.append(cgmax)
+     
+        
+    cg_fwd = (np.min(cgmin_lst) - x_lemac) / MAC * 0.98      #subtract 2% margin, assuming most forward cg is after lemac
+    cg_aft = (np.max(cgmax_lst) - x_lemac) / MAC *1.02      #add 2% margin
+    print("most forward cg should be positive")
+   
+    
+    return cg_fwd, cg_aft
 
 
-cgMTOW, extreme_cg = loading()  # extreme cg is in % of mac, first fwd, then aft, fully loaded is cg
-
-
-#calculate extremes and add 2% margin on both sides
-#vary wing position
-
-
-
-
-
-
-
-
-
-
-
-# tail_armh = xlemac_horizontal + 0.25 * MAC_horizontal - cgMTOW
-# tail_armv = xlemac_vertical + 0.25 * MAC_vertical - cgMTOW
-# C_h = horizontal_area * tail_armh / (wing_area * MAC)
-# C_v = vertical_area * tail_armv / (wing_area * span)
-
-# clalpha = (1 - -1) / 17.5
-# cmalpha = (0.0329 + 0.0093) / (7.25 + 13.75)
-# cl0 = -1.25  # degrees
-# cm0 = -0.0184
-# area_fuselage = diameter * 14 * (10 / 21)
-# mach = 0.78
-# v_approach = 129  # kts
-# beta = (1 - 0.85 * (mach ** 2)) ** 0.5
-# n = 0.95
-# stabilizer_sweep = np.radians(27)
-# span_tail = 12.3
-# AR_tail = span_tail ** 2 / horizontal_area
-# AR = span ** 2 / wing_area
-# sweep = np.radians(24.5)
-
-# clalpha_datcom = 2 * np.pi * AR / (2 + ((4 + ((AR * beta) ** 2) * (1 + (np.tan(sweep) ** 2) / beta ** 2)) ** 0.5))
-
-# clalpha_tail = 2 * np.pi * AR_tail / (
-#             2 + ((4 + ((AR_tail * beta / n) ** 2) * (1 + (np.tan(stabilizer_sweep) ** 2) / beta ** 2)) ** 0.5))
-# clalpha_tail_degrees = np.radians(clalpha_tail)
-
-# clalpha_tailless = clalpha * (1 + 2.15 * (diameter / span)) * (wing_area - area_fuselage) / wing_area + (
-#             np.pi * diameter ** 2) / (2 * wing_area)
-
-# # tail-wing speed ratio
-# v_hv = 0
-
-# # data_weight = { "Variable": ['MTOW', 'OEW', 'Payload', 'Fuel weight','Pax & cabin luggage', 'Forward cargo', 'Aft cargo'],
-# #                 'CS100': [MTOW, OEW, PL, fuel_weight, pax_cabin, fwd_cargo_max, aft_cargo_max],
-# #                 'Percentage of CS100 MTOW':[100, OEW_percent, PL_percent, fuel_weight_percent, pax_cabin_percent, 100*fwd_cargo_max/MTOW, 100*aft_cargo_max/MTOW]
-# #                     }
-
-# # table_weight = pd.DataFrame(data_weight, columns = ['Variable', 'Value', 'Percentage of MTOW']).to_latex(caption = 'caption', label = 'label', column_format= '|l|l|l|', multirow = True, float_format="%.2f", index = False)
-
-# # slash = "\\"
-# # table_weight = table_weight.replace(slash+slash, slash+slash + ' '+slash+"hline" ).replace("toprule",'hline').replace(".00",'')
-
-
-# # data_cg = {"CG": ['Forward CG','Aft CG',  'OEW', 'Fuel', 'Forward hold', 'Aft hold', 'Main landing gear'],
-# #            "% of MAC": [ extreme_cg[0],extreme_cg[1], cgoew_mac, maccie(cgfuel), maccie(fwdcargo_cg), maccie(aftcargo_cg), maccie(cgmain)],
-# #            'Condition':["Aft cargo loaded", 'Fwd cargo loaded', 'Empty', '', '', '', '']}
-
-# # table_cg = pd.DataFrame(data_cg, columns = ["CG", "% of MAC", 'Condition']).to_latex(caption = 'caption', label = 'label', column_format= '|l|l|l|', multirow = True, float_format="%.2f", index = False)
-
-# # slash = "\\"
-# # table_cg = table_cg.replace(slash+slash, slash+slash + ' '+slash+"hline" ).replace("toprule",'hline').replace(".00",'')
-
-# # scissor_required = [mach, v_approach, v_hv, clalpha_tailless, 'C_{l'+slash+'alpha_{t-h}}_{wing}','C_{l'+slash+'alpha_{t-h}}_{fuselage}', 'wing downwash gradient','AC_{t-h_{cruise}}',  'AC_{t-h_{cruise_{wing}}}', 'AC_{t-h_{cruise_{fuselage}}}','AC_{t-h_{approach}}', 'AC_{t-h_{approach_{wing}}}', 'AC_{t-h_{approach_{fuselage}}}', 'AC_{t-h_{nacelles}}', 'C_{l_{t}max}', 'C_{m0}' , 'C_{m0_{wing}}', 'C_{m0_{flaps}}' , 'C_{m0_{fuselage}}', 'C_{m0_{nacelles}}']
-# # data_scissor = {'Variable':['Cruise speed', 'Approach speed', 'Tail-wing speed ratio', 'C_{l'+slash+'alpha_{t}}', 'C_{l'+slash+'alpha_{t-h}}_{wing}','C_{l'+slash+'alpha_{t-h}}_{fuselage}', 'wing downwash gradient','AC_{t-h_{cruise}}',  'AC_{t-h_{cruise_{wing}}}', 'AC_{t-h_{cruise_{fuselage}}}','AC_{t-h_{approach}}', 'AC_{t-h_{approach_{wing}}}', 'AC_{t-h_{approach_{fuselage}}}', 'AC_{t-h_{nacelles}}', 'C_{l_{t}max}', 'C_{m0}' , 'C_{m0_{wing}}', 'C_{m0_{flaps}}' , 'C_{m0_{fuselage}}', 'C_{m0_{nacelles}}'],
-# #                 'New':['Cruise speed', 'Approach speed', 'Tail-wing speed ratio', 'C_{l'+slash+'alpha_{t}}', 'C_{l'+slash+'alpha_{t-h}}_{wing}','C_{l'+slash+'alpha_{t-h}}_{fuselage}', 'wing downwash gradient','AC_{t-h_{cruise}}',  'AC_{t-h_{cruise_{wing}}}', 'AC_{t-h_{cruise_{fuselage}}}','AC_{t-h_{approach}}', 'AC_{t-h_{approach_{wing}}}', 'AC_{t-h_{approach_{fuselage}}}', 'AC_{t-h_{nacelles}}', 'C_{l_{t}max}', 'C_{m0}' , 'C_{m0_{wing}}', 'C_{m0_{flaps}}' , 'C_{m0_{fuselage}}', 'C_{m0_{nacelles}}'],
-# #                 'Units':['Cruise speed', 'Approach speed', 'Tail-wing speed ratio', 'C_{l'+slash+'alpha_{t}}', 'C_{l'+slash+'alpha_{t-h}}_{wing}','C_{l'+slash+'alpha_{t-h}}_{fuselage}', 'wing downwash gradient','AC_{t-h_{cruise}}',  'AC_{t-h_{cruise_{wing}}}', 'AC_{t-h_{cruise_{fuselage}}}','AC_{t-h_{approach}}', 'AC_{t-h_{approach_{wing}}}', 'AC_{t-h_{approach_{fuselage}}}', 'AC_{t-h_{nacelles}}', 'C_{l_{t}max}', 'C_{m0}' , 'C_{m0_{wing}}', 'C_{m0_{flaps}}' , 'C_{m0_{fuselage}}', 'C_{m0_{nacelles}}']}
-
-# # table_scissor = pd.DataFrame(data_scissor, columns = ["Variable", "New", 'Units']).to_latex(escape = False, caption = 'caption', label = 'label', column_format= '|l|l|l|', multirow = True, float_format="%.2f", index = False)
-
-# # slash = "\\"
-# # table_scissor= table_scissor.replace(slash+slash, slash+slash + ' '+slash+"hline" ).replace("toprule",'hline').replace(".00",'')
-
-
-# # data_wing = {'Variable':["MAC", "XLEMAC","MAC_{horizontal}", "XLEMAC_{horizontal}","MAC_{vertical}", "XLEMAC_{vertical}", "Horizontal tail arm","Vertical tail arm", "S_{horizontal}", "S_{horizontal}/S_{wing}", "S_{vertical}", "S_{vertical}/S_{wing}", "C_h", "C_v", "X_{nose gear}","X_{main gear}", "X_{main gear_{MAC}}", "b", "b_{horizontal}"],
-# #              'CS100':[MAC, xlemac,MAC_horizontal, xlemac_horizontal,MAC_vertical, xlemac_vertical, tail_armh,tail_armv, horizontal_area, horizontal_area/wing_area, vertical_area, vertical_area/wing_area, C_h, C_v, cgnose, cgmain, maccie(cgmain), span, span_tail],
-# #              'New':[MAC, xlemac,MAC_horizontal, xlemac_horizontal,MAC_vertical, xlemac_vertical, tail_armh,tail_armv, horizontal_area, horizontal_area/wing_area, vertical_area, vertical_area/wing_area, C_h, C_v, cgnose, cgmain, maccie(cgmain), span, span_tail]}
-
-
-# # table_wing = pd.DataFrame(data_wing, columns = ["Variable", "CS100", 'New']).to_latex(escape = False, caption = 'caption', label = 'label', column_format= '|l|l|l|', multirow = True, float_format="%.2f", index = False)
-
-# # slash = "\\"
-# # table_wing= table_wing.replace(slash+slash, slash+slash + ' '+slash+"hline" ).replace("toprule",'hline').replace(".00",'')
-
-# # print(table_wing)
-
-# # cars = {'Brand': ['Honda Civic','Toyota Corolla','Ford Focus','Audi A4'],
-# #         'Price': [22000,25000,27000,35000]
-# #         }
-
-# # df = pd.DataFrame(cars, columns = ['Brand', 'Price'])
-
-
-# # ltx = df.to_latex(caption = 'caption', label = 'label', column_format= '|l|l|l|', multirow = True)
-# # slash = "\\"
-# # print(ltx.replace(slash+slash, slash+slash + ' '+slash+"hline" ))
-
-
-# # hundredpercent = OEW_percent + pax_cabin_percent+fuel_weight_percent+cargo_percent
-
-# # percentages = [OEW_percent, pax_cabin_percent,fwd_cargo_percent, aft_cargo_percent, fuel_weight_percent]
-# # labels = 'OEW', 'Passengers', 'Fwd Cargo', 'Aft Cargo','Fuel'
-
-# #     #make py chart
-# # def pychart():
-# #     fig1, ax1 = plt.subplots()
-# #     ax1.pie(percentages,  labels=labels, autopct='%1.1f%%', startangle=90)
-# #     ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-# #     plt.show()
-
-# # wing_area_ft = 112.3*10.7639104
-# # MTOW_lbs = MTOW * 2.2046226218488
-# # vertical_area_ft = vertical_area *10.7639104
-# # horizontal_area_ft = horizontal_area *10.7639104
-
-# # l_tail = 4.9+2.5
-# # l_nose = 34.9/12.5 *2
-# # s_wetted = np.pi * diameter * (34.1-l_tail -l_nose ) + (l_nose+l_tail)*diameter*0.5*np.pi
-
-
-# # weight calculation
-# # w_wing = 10*wing_area_ft*0.45359237
-# # w_horizontal = 5.5* horizontal_area_ft*0.45359237
-# # w_vertical = 5.5 *vertical_area_ft*0.45359237
-# # w_fuselage = 5* s_wetted*0.45359237 *10.7639104
-# # w_noselandinggear = 0.043*0.15*MTOW
-# # w_mainlandinggear = 0.043*0.85*MTOW
-# # w_engineinstalled = 1.3*w_engine
-# # all_else = 0.17*MTOW
-
-# # calculated_OEW = w_wing+w_horizontal+w_vertical+w_noselandinggear+w_mainlandinggear+w_engineinstalled+all_else +w_fuselage
-# # weight_multiplication_factor = OEW/calculated_OEW
-
-# # w_wing = 10*wing_area_ft*0.45359237 * weight_multiplication_factor
-# # w_horizontal = 5.5* horizontal_area_ft*0.45359237* weight_multiplication_factor
-# # w_vertical = 5.5 *vertical_area_ft*0.45359237* weight_multiplication_factor
-# # w_fuselage = 5* s_wetted*0.45359237 *10.7639104* weight_multiplication_factor
-# # w_noselandinggear = 0.043*0.15*MTOW* weight_multiplication_factor
-# # w_mainlandinggear = 0.043*0.85*MTOW* weight_multiplication_factor
-# # w_engineinstalled = 1.3*w_engine* weight_multiplication_factor
-# # all_else = 0.17*MTOW* weight_multiplication_factor
-
-# # cg calculations
-# # cgwing = 0.4*MAC + xlemac
-# # cghorizontal = 0.4*MAC_horizontal+xlemac_horizontal
-# # cgvertical = 0.4*MAC_vertical+xlemac_vertical
-# # cgfuselage = 0.4*fuselage_lenght
-# # cgnose = 3.5
-# # cgengine = 10+ 4*10/21
-# # cgallelse = 0.4*fuselage_lenght
-
-
-# # xcgoew = (w_wing*cgwing + w_horizontal * cghorizontal + w_vertical*cgvertical + w_noselandinggear*cgnose + w_mainlandinggear*cgmain + w_engineinstalled*cgengine + all_else*cgallelse + w_fuselage*cgfuselage)/OEW
-
-# # cgoew_mac = 100*(xcgoew-xlemac)/MAC
-# # print(cgoew_mac)
+cg_fwd, cg_aft = loading()  
+print('CG range:', cg_fwd, '-', cg_aft, 'MAC')
