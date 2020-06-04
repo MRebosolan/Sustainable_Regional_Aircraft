91f59a5ee8f26756613788be72d14e2431d20247
AQUA7XICE <61149150+AQUA7XICE@users.noreply.github.com>
2020-06-04 10:03:50 +0200

    Update cabindesign.py

--- cabindesign.py
+++ cabindesign.py
@@ -4,52 +4,94 @@ Created on Wed Jun  3 13:43:05 2020
 
 @author: malfl
 """
+
+def cabin_design(fractioninfus,fractionintail,HYDROGENVOLUME):
+    import numpy as np
+
+    import hydrogen_tank_sizing
+    #PASSENGER SECTION
+    #LARGELY BASED ON AIRBUS A220
+    
+    
+    wseat=18.9*0.0254
+    warmrest=2*0.0254
+    waisle=0.5
+    wclearance=0.01
+    seat_pitch=32*0.0254
+    emergency_clearance=10*0.0254
+    
+    h_aisle=2
+    
+    hheadroom=1.5
+    totalabovefloor=0.68
+    wall_thickness=0.1
+    floor_thickness=0.1
+    
+    three=round(wseat*3+wclearance+warmrest*4,3)
+    two=round(wseat*2+wclearance+warmrest*3,3)
+    
+    total=round(two+three+waisle,3)
+    wheadroom=round(total-2*(wclearance+warmrest)-wseat,3)
+    wfloor=round(total-0.18,3)
+    
+    
+    
+    #two types I exits
+    #two types III exits
+    paxsectionlength=round(15*seat_pitch+emergency_clearance+wseat,3) #
+    aft_galley_length=1
+    front_galley_length=1.8
+    totalcabinlength=paxsectionlength+aft_galley_length+front_galley_length
+    
+    outer_diameter=3.486
+    inner_diameter=3.286
+    
+    V_tank=30 #CLASS I
+    R_tank_fus=outer_diameter/2-0.15
+    R_tank_tail=1
+    
+
+        
+        
+    
+        
+    V_tank_cyl=(fractioninfus-fractionintail*fractioninfus)*V_tank
+    V_tank_tail=fractionintail*fractioninfus*V_tank
+    V_tank_top=V_tank-V_tank_cyl-V_tank_tail
+        
+    #CYLINDER STORAGE
+    t_cyl,m_cyl, tm_cyl, d_cyl,l_cyl=tank_sizing_fuselage(V_tank_cyl,R_tank_fus,1)
+    if V_tank_cyl==0:
+        t_cyl,m_cyl, tm_cyl, d_cyl,l_cyl=0,0,0,0,0    
+    print('CYLINDER TANK: ','| mass: ',tm_cyl,'| diameter: ',d_cyl,'| length: ',l_cyl)
+    
+    #TAIL STORAGE
+    t_tail,m_tail, tm_tail, d_tail,l_tail=tank_sizing_fuselage(V_tank_tail,R_tank_tail,1)
+    if V_tank_tail==0:
+        t_tail,m_tail, tm_tail, d_tail,l_tail=0,0,0,0,0     
+    print('TAIL TANK: ','| mass: ',tm_tail,'| diameter: ',d_tail,'| length: ',l_tail)
+    
+    #TOP STORAGE
+    t_top,m_top,tm_top,d_top,l_top=tank_sizing(V_tank_top,totalcabinlength+l_cyl,2)
+    if V_tank_top==0:
+        t_top,m_top, tm_top, d_top,l_top=0,0,0,0,0    
+    print('TOP TANK: ','| mass: ',tm_top,'| diameter: ',d_top,'| length: ',l_top)
+    
+
+
+    return(t_top, m_top, tm_top, d_top, l_top, t_cyl, m_cyl, tm_cyl, d_cyl, l_cyl, t_tail, m_tail, tm_tail, d_tail, l_tail,totalcabinlength,V_tank_cyl,V_tank_tail,V_tank_top)
+    
+
+t_top, m_top, tm_top, d_top, l_top, t_cyl, m_cyl, tm_cyl, d_cyl, l_cyl, t_tail, m_tail, tm_tail, d_tail, l_tail,totalcabinlength,V_tank_cyl,V_tank_tail,V_tank_top=cabin_design(0,0,30)
+
+total_mass=tm_top+tm_cyl+tm_tail
+CGalltanks=((tm_top)*(totalcabinlength+l_cyl)/2+(tm_cyl)*(totalcabinlength+l_cyl/2)+(tm_tail)*(totalcabinlength+l_cyl+l_tail/2))/total_mass #CG FROM THE START OF THE CABIN (i.e. front of front galley)
+
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-import hydrogen_tank_sizing
-#PASSENGER SECTION
-#LARGELY BASED ON AIRBUS A220
-
-wseat=18.9*0.0254
-warmrest=2*0.0254
-waisle=0.5
-wclearance=0.01
-seat_pitch=32*0.0254
-emergency_clearance=10*0.0254
-
-h_aisle=2
-
-hheadroom=1.5
-totalabovefloor=0.68
-wall_thickness=0.1
-floor_thickness=0.1
-
-three=round(wseat*3+wclearance+warmrest*4,3)
-two=round(wseat*2+wclearance+warmrest*3,3)
-
-total=round(two+three+waisle,3)
-wheadroom=round(total-2*(wclearance+warmrest)-wseat,3)
-wfloor=round(total-0.18,3)
-
-
-
-#two types I exits
-#two types III exits
-paxsectionlength=round(15*seat_pitch+emergency_clearance,3)
-aft_galley_length=1.5
-front_galley_length=2
-totalcabinlength=paxsectionlength+aft_galley_length+front_galley_length
-
-outer_diameter=3.486
-inner_diameter=3.286
-
-V_tank=30 #CLASS I
-R_tank_fus=1.5
-R_tank_tail=1
-
 #something for plotting in 3D
 def set_axes_radius(ax, origin, radius):
     ax.set_xlim3d([origin[0] - radius, origin[0] + radius])
@@ -74,30 +116,7 @@ def set_axes_equal(ax):
     origin = np.mean(limits, axis=1)
     radius = 0.5 * np.max(np.abs(limits[:, 1] - limits[:, 0]))
     set_axes_radius(ax, origin, radius)
-    
-    
-
-
-fractioninfus=0 #FRACTION OF FUEL IN FUSELAGE (CYLINDER AND TAIL)
-fractionintail=0 #FRACTION OF FUSELAGE FUEL IN TAIL
-    
-V_tank_cyl=(fractioninfus-fractionintail*fractioninfus)*V_tank
-V_tank_tail=fractionintail*fractioninfus*V_tank
-V_tank_top=V_tank-V_tank_cyl-V_tank_tail
-    
-#CYLINDER STORAGE
-t_cyl,m_cyl, tm_cyl, d_cyl,l_cyl=tank_sizing_fuselage(V_tank_cyl,R_tank_fus,1)
-print('CYLINDER TANK: ','| mass: ',tm_cyl,'| diameter: ',d_cyl,'| length: ',l_cyl)
-
-#TAIL STORAGE
-t_tail,m_tail, tm_tail, d_tail,l_tail=tank_sizing_fuselage(V_tank_tail,R_tank_tail,1)
-print('TAIL TANK: ','| mass: ',tm_tail,'| diameter: ',d_tail,'| length: ',l_tail)
-
-#TOP STORAGE
-t_top,m_top,tm_top,d_top,l_top=tank_sizing(V_tank_top,totalcabinlength+l_cyl,2)
-print('TOP TANK: ','| mass: ',tm_top,'| diameter: ',d_top,'| length: ',l_top)
-
-
+outer_diameter=3.486
 alpha=np.linspace(0,2*np.pi,40)
 x=np.cos(alpha)*outer_diameter/2
 y=np.sin(alpha)*outer_diameter/2
@@ -126,6 +145,3 @@ ax.plot_surface(xtail, ytail, ztail, color='b')
 ax.set_aspect('equal')
 
 set_axes_equal(ax)             # important!
-
-    
-
