1b76746ab0cfecbf3573cc95fc2f32eb87717d2f
mrebosolan <superreba17@gmail.com>
2020-06-05 08:28:00 +0200

    fuselage shear&moment diagrams

--- Fuselage_structural_design.py
+++ Fuselage_structural_design.py
@@ -1,19 +1,27 @@
 import input
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 """
 Written by Matteo & Manuel
 LOADS ACTING ON FUSELAGE: pressure vessel stresses, fuselage's own weight (assumed uniformly distributed
 wing weight, empennage weight, landing gear reaction forces.
 
-
+#ASSUMPTIONS
+- Uniform longitudinal weight distribution
+- Tail lift applied at end of aircraft length
 """
 
 P_c = input.P_c
 r = input.widthf/2
 z_vl = input.bv/2 #point of action of vertical tail lift, estimated at half vert.tail height
 t_options = np.linspace(0, 0.30, 1000)[1:]
+W_cruise = 0.985 * input.MTOW
+ac_length = 30 #dummy value
+x_ac = input.x_ac
+lh = input.lh
+
+x_array = np.linspace(0, ac_length, 1000)
 
 
 
@@ -37,16 +45,32 @@ def shear_flow_due_to_empennage(rudder_load, enclosed_area, z_vl=z_vl):
     shear_flow = torque/(2*enclosed_area) #right hand positive
     return shear_flow
 
-
-def internal_shear_and_moment_longitudinal(x, W_f, lf, x_ac):
-    w_dist = W_f/
+def internal_shear_and_moment_longitudinal(x, W= 322111, lf=ac_length, x_ac=x_ac, xh=ac_length):
+    w_dist = W/lf
+    L = ((0.5*lf)/(lf-x_ac))*W
+    Lh = W-L
     if x>x_ac:
-        d= x-x_ac
+        d1, d2= 1,x-x_ac
     else:
-        d=0
-    shear_at_x =
+        d1, d2=0,0
+    if x>=xh:
+        d3,d4 = 1, x-xh
+    else:
+        d3,d4=0,0
 
+    shear_at_x = -w_dist*x + L*d1 + Lh*d3
+    moment_at_x = -w_dist*(0.5*x**2) + L*d2 + Lh*d4
+    return shear_at_x, moment_at_x
 
+moments=[]
+shears=[]
+for x in x_array:
+    moments.append(internal_shear_and_moment_longitudinal(x)[1])
+    shears.append(internal_shear_and_moment_longitudinal(x)[0])
+
+plt.plot(x_array, moments)
+plt.plot(x_array, shears)
+plt.show()
 
 # location of the systems from nose
 x_mg = 15 #main gear from nose in m
@@ -64,9 +88,9 @@ fuselage_length = 40
 
 
 #Reaction force at the main gear
-Reaction_MG = (w_emp(x_emp-x_ng)+w_wg(x_wg-x_ng)+(mtow-w_emp-w_wg)*(fuselage_length/2-x_ng))/(x_mg-x_ng)
+#Reaction_MG = (w_emp(x_emp-x_ng)+w_wg(x_wg-x_ng)+(mtow-w_emp-w_wg)*(fuselage_length/2-x_ng))/(x_mg-x_ng)
 #Reaction force at the nose landing gear
-Reaction_NG = mtow-Reaction_MG
+#Reaction_NG = mtow-Reaction_MG
 
 
 
