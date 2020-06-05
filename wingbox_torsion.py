0cbe2110946565fd1e91d5006374df717ed96364
Montse Barbera <montsebarbera@MacBook-Pro-de-Montse.local>
2020-06-05 17:59:11 +0200

    update

--- wingbox_torsion.py
+++ wingbox_torsion.py
@@ -2,7 +2,43 @@
 import input
 import Wingbox_design
 
+x_array = Wingbox_design.x_array[2:]
+moment_array = Wingbox_design.moment_array
+moment_array2 = Wingbox_design.moment_array2
 
-x_array = Wingbox_design.x_array
-yield_stress_material = 200
+ # coordinates for the airfoil
+yield_stress_material = 200 # start of with steel for now
+
+# Read from file
+
+f1 = open('airfoil2.txt', 'r')
+lines1 = f1.readlines()
+
+xcoord1 = []
+ycoord1 = []
+xcoord2 = []
+ycoord2 = []
+
+for i in range(0, 103):
+
+    xcoord1.append(float(lines1[i].split()[0]))
+    ycoord1.append(float(lines1[i].split()[1]))
+
+
+
+for i in range(103, 205):
+    xcoord2.append(float(lines1[i].split()[0]))
+    ycoord2.append(float(lines1[i].split()[1]))
+
+
+
+xcoord2.insert(-1, 1.0)
+xcoord2 = xcoord2[::-1]
+ycoord2.insert(-1, 0.0)
+xcoord2 = xcoord2[::-1]
+
+print(xcoord1)
+print(xcoord2)
+
+
 
