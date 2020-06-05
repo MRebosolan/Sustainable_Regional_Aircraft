a7e899f525e7b7c3a6d28fd9915a180d288a0018
AQUA7XICE <61149150+AQUA7XICE@users.noreply.github.com>
2020-06-05 13:46:20 +0200

    ff

--- tanktradeoffdrag.py
+++ tanktradeoffdrag.py
@@ -9,17 +9,31 @@ from cabindesign import cabin_design
 
 finfuslist=[]
 cdlist=[]
-
+cd2list=[]
+totdraglist=[]
+lcyllist=[]
 for finfus in range(0,11):
-    finfus/=100
+    finfus/=10
     t_cyl,m_cyl, tm_cyl, d_cyl,l_cyl,t_tail,m_tail, tm_tail, d_tail,l_tail\
            ,t_top,m_top,tm_top,d_top,l_top,totalcabinlength,V_tank_cyl, V_tank_tail, V_tank_top,\
-           tm_tanksystem,CGtank,CGfuelfull,CGcomb,totdrag,fuselage_weight,CDzerofus,FFbody,Cfturb,fuselage_area=cabin_design(finfus,0,25,1)  
-    cdlist.append(CDzerofus)
+           tm_tanksystem,CGtank,CGfuelfull,CGcomb,totdrag,fuselage_weight,CDzerofus,FFbody,Cfturb,fuselage_area,CDzeropods,fusdrag,poddrag=cabin_design(finfus,0,25,1)  
+    cdlist.append(totdrag)
     finfuslist.append(1-finfus)
-
+    t_cyl,m_cyl, tm_cyl, d_cyl,l_cyl,t_tail,m_tail, tm_tail, d_tail,l_tail\
+           ,t_top,m_top,tm_top,d_top,l_top,totalcabinlength,V_tank_cyl, V_tank_tail, V_tank_top,\
+           tm_tanksystem,CGtank,CGfuelfull,CGcomb,totdrag,fuselage_weight,CDzerofus,FFbody,Cfturb,fuselage_area,CDzeropods,fusdrag,poddrag=cabin_design(finfus,0,25,0)  
+    cd2list.append(fusdrag)
+    lcyllist.append(l_cyl)
+    totdraglist.append(totdrag)
+    
 import matplotlib.pyplot as plt
 
-plt.plot(finfuslist,cdlist)
+plt.subplot(121)
+plt.plot(finfuslist,cdlist,label='total drag toptank')
+plt.plot(finfuslist,cd2list,label='drag fuselage of pods')
+plt.plot(finfuslist,totdraglist,label='total drag pods')
+plt.legend()
+plt.subplot(122)
+plt.plot(finfuslist,lcyllist)
 plt.show()
     
