5c666850b103ca9c84128b726993f4bc59da26c9
mrebosolan <superreba17@gmail.com>
2020-06-09 16:02:34 +0200

    wingbox final file added

---
+++ Wingbox_mass.py
@@ -0,0 +1,9 @@
+
+
+#Needed: #of booms,
+
+
+def determine_necessary_moi():
+    y_max = 1 #maximum vertical position of airfoil section
+    sigma_yield = 300E6 #yield strength of material
+    sigma_cr =
