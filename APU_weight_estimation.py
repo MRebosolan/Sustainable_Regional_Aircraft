bdbd0c80bd5ca78951312426d5556a0afc78d03d
martijnvroom <martijnv17@gmail.com>
2020-06-12 09:05:00 +0200

    ''

--- APU_weight_estimation.py
+++
@@ -1,8 +0,0 @@
-''' 
-to calculate apu weight based on take off weight
-outputs apu weight
-responsible: matteo'''
-def APU_weight_estimation(Wto):
-    #Wto = take-off weight
-    Wapu = 0.0075 * Wto
-    return Wapu
