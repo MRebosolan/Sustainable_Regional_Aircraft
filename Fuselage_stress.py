fa288fc26e5ef51aaa75b20db89ef97a9a5d29b0
Montse Barbera <montsebarbera@MacBook-Pro-de-Montse.local>
2020-06-12 09:41:29 +0200

    dd

--- Fuselage_stress.py
+++ Fuselage_stress.py
@@ -1,13 +1,16 @@
 import numpy as np
+import input
 
 #------------------------------------------------------------------------------------------------------------------
 #INPUTS
 
-R_f = 2 # Fuselage radius
-t_f = 0.002 #THICkNESS OF FUSELAGE
-number_boom = 10 #NUMBER OF BOOMS
-moment_cs = 1100000 #MOMENT OF FUSELAGE CROSS SECTION
+widthf = input.widthf
+R_f = widthf/2  # Fuselage radius
+t_f = 0.001 #THICkNESS OF FUSELAGE
+number_boom = 8 #NUMBER OF BOOMS
+moment_cs = 700000 #MOMENT OF FUSELAGE CROSS SECTION
 shear_cs = 150000 #SHEAR FORCE OF THE CROSS SECTION
+pressure_d = 52050
 
 #------------------------------------------------------------------------------------------------------------------
 
@@ -28,21 +31,21 @@ for i in range(number_boom):
 #with these two angle we can find the boom_locationx and the boom_locationy
 
 b = 2*np.pi*R_f/number_boom
-
+stringer_area = 0.001
 
 for i in range(len(boom_locationx)):
     if i == 0:
-        area_boom = t_f * b / 6 + t_f * b * (2 + boom_locationy[i + 1] / boom_locationy[i]) / 6
+        area_boom = stringer_area + t_f * b / 6 + t_f * b * (2 + boom_locationy[i + 1] / boom_locationy[i]) / 6
         moi_boom = area_boom * boom_locationy[i] ** 2
         boom_area.append(area_boom)
         boom_moi.append(moi_boom)
     elif i == len(boom_locationx)-1:
-        area_boom = t_f * b * (2 + boom_locationy[i - 1] / boom_locationy[i]) / 6 + t_f * b / 6
+        area_boom = stringer_area + t_f * b * (2 + boom_locationy[i - 1] / boom_locationy[i]) / 6 + t_f * b / 6
         moi_boom = area_boom * boom_locationy[i]  ** 2
         boom_area.append(area_boom)
         boom_moi.append(moi_boom)
     if i > 0 and i < len(boom_locationx)-1:
-        area_boom = t_f*b*(2+boom_locationy[i-1]/boom_locationy[i])/6+t_f*b*(2+boom_locationy[i+1]/boom_locationy[i])/6
+        area_boom = stringer_area + t_f*b*(2+boom_locationy[i-1]/boom_locationy[i])/6+t_f*b*(2+boom_locationy[i+1]/boom_locationy[i])/6
         moi_boom = area_boom*boom_locationy[i]**2
         boom_area.append(area_boom)
         boom_moi.append(moi_boom)
@@ -50,7 +53,7 @@ for i in range(len(boom_locationx)):
 moi_boom_total = sum(boom_moi)
 
 for i in range(len(boom_locationx)):
-        stress_boom = moment_cs*boom_locationy[i] / moi_boom_total
+        stress_boom = moment_cs*boom_locationy[i] / moi_boom_total + pressure_d*R_f/(2*t_f)
         boom_stress.append(stress_boom)
 
 # print(boom_stress)
@@ -61,7 +64,7 @@ for i in range(len(boom_locationx)):
     deltashear_boom = shear_cs*boom_area[i]*boom_locationy[i]/moi_boom_total
     boom_deltashear.append(deltashear_boom)
 
-# print(boom_deltashear)
+#print(boom_deltashear)
 
 
 shear_flow1 = []
@@ -71,15 +74,21 @@ shear_stress = []
 shearflow = 0
 shear_flow1.append(shearflow)
 
-for i in range(len(boom_locationx[int(number_boom/4):])):
-    shearflow = shearflow + boom_deltashear[int(number_boom/4)+i]
-    shear_flow1.append(shearflow)
-
-for i in range(len(boom_locationx[:int(number_boom/4)])-1):
-    shearflow = shearflow + boom_deltashear[i]
-    shear_flow2.append(shearflow)
-
-shear_flow = shear_flow2+shear_flow1
-shear_stress = np.array(shear_flow)/t_f
-
-
+for i in range(len(boom_locationx)):
+    if boom_locationx[i] < 0:
+        shearflow = shearflow + boom_deltashear[i]
+        shear_flow2.append(shearflow)
+    if i<len(boom_locationx)/2 and boom_locationx[i] > 0:
+     print(boom_locationx)
+#     print(i)
+#     [int(number_boom/4)+i]
+#     shear_flow1.append(shearflow)
+#
+# for i in range(len(boom_locationx[:int(number_boom/4)])-1):
+#     shearflow = shearflow + boom_deltashear[i]
+#     shear_flow2.append(shearflow)
+#
+# shear_flow = shear_flow2+shear_flow1
+# shear_stress = np.array(shear_flow)/t_f
+#
+# #print(shear_stress)
